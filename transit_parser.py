a29cbbd88981c4a2ec875530cb1eceaa5b5951d2
Pranav Badami <pbadami@pb-fvfv1ckfj1wl.local>
2018-01-29 16:19:14 -0500

    fixed get_stop_times to work for all cases, supported trains where data ends prematurely

--- transit_parser.py
+++ transit_parser.py
@@ -8,7 +8,7 @@ class TrainParser:
 	def __init__(self, filename):
 		self.filename = filename
 		self.data = self.read(self.filename)
-
+		self.train, self.line, self.type = self.get_meta()
 
 	def read(self, filename):
 		try:
@@ -17,16 +17,15 @@ class TrainParser:
 			print "error reading {}".format(filename)
 			return None
 
-	def get_train_meta(self):
-		pass
+	def get_meta(self):
+		return self.data['id'], self.data['line'], self.data['type']
 
 	def get_stop_times(self):
 		dep_count = 0
 		departures = []
 		for frame in self.data['data']:
 			time = frame[0]
-			stops = frame[1]
-			stops = stops[dep_count:]
+			stops = frame[1][dep_count:]
 			for idx, stop in enumerate(stops):
 				try:
 					station, status = stop.split(u"\xa0\xa0")
@@ -34,18 +33,41 @@ class TrainParser:
 					station = ""
 					status = ""
 
-				if station not in ALL_STATIONS:
-					pass
-				elif ("DEPARTED" in status):
-					departures.append({'station': station,
-									   'time': time,
-									   'status': "Departed"})
+				if ("DEPARTED" in status):
+					if station in ALL_STATIONS:
+						departures.append({'station': station,
+										   'time': time,
+										   'status': "Departed"})
 					dep_count = dep_count + 1
 				elif ("Cancelled" in status):
-					departures.append({'station': station,
+					if station in ALL_STATIONS:
+						departures.append({'station': station,
 									   'time': time,
 									   'status': "Cancelled"})
 					dep_count = dep_count + 1
+
+		if len(departures) < len(self.data['data'][0][1]):
+			for stop in self.data['data'][0][1][len(departures)+1:]:
+				try:
+					station, status = stop.split(u"\xa0\xa0")
+				except ValueError:
+					station = ""
+					status = ""
+				if station in ALL_STATIONS:
+					departures.append({'station': station,
+									   'time': time,
+									   'status': None})
 		return departures
+
+	def format_as_csv(self, departures):
+		prev = departures[0]
+
+		for departure in departures:
+			row = [self.train, self.line, self.type]
+			row = row + [prev['station'], departure['station'], departure['time'], departure['status']]
+			prev = departure
+			print row
+
+
 
 
